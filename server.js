874777973fab60a4400c31e9ca1fa1df5a8a310b
tgp0315 <guiguzi0315@163.com>
2022-09-16 00:23:08 +0800

    feat:添加生成静态资源

--- server.js
+++ server.js
@@ -9,56 +9,56 @@ const __dirname = dirname(__filename)
 
 
 async function createSeaver(isProd = process.env.NODE_ENV === 'production') {
-    const app = express()
-    const resolve = p => path.resolve(__dirname, p)
+  const app = express()
+  const resolve = p => path.resolve(__dirname, p)
+  const manifest = isProd ? await import('./dist/static/ssr-manifest.json') : {}
+  const indexProd = isProd ? fs.readFileSync(resolve('./dist/client/index.html'), 'utf-8') : ''
+  let vite
+  if (!isProd) {
+      vite = await createViteServer({
+          server: {
+              middlewareMode: 'ssr'
+          }
+      })
+      app.use(vite.middlewares)
+  } else {
+      app.use(serveStatic(resolve('dist/client'), {
+          index: false
+      }))
+  }
 
-    const indexProd = isProd ? fs.readFileSync(resolve('./dist/client/index.html'), 'utf-8') : ''
-    let vite
-    if (!isProd) {
-        vite = await createViteServer({
-            server: {
-                middlewareMode: 'ssr'
-            }
-        })
-        app.use(vite.middlewares)
-    } else {
-        app.use(serveStatic(resolve('dist/client'), {
-            index: false
-        }))
+
+  app.use('*', async (req, res) => {
+    const url = req.originalUrl
+    let template;
+    let render;
+    try {
+      if (!isProd) {
+        template = fs.readFileSync(resolve('./index.html'), 'utf-8')
+        template = await vite.transformIndexHtml(url, template)
+        render = (await vite.ssrLoadModule('./src/entry-server.js')).render
+      } else {
+        template = indexProd
+        const entryServe = await import('./dist/server/entry-server.js')
+        render = entryServe.render
+      }
+      const appHtml = await render(url, manifest)
+      // 5. 注入渲染后的应用程序 HTML 到模板中。
+      const html = template.replace(`<!--ssr-outlet-->`, appHtml)
+      // // 6. 返回渲染后的 HTML。
+      res.status(200)
+      res.setHeader('Content-Type', 'text/html')
+      res.end(html)
+    } catch (e) {
+      vite && vite.ssrFixStacktrace(e)
+      res.status(500).end(e.message)
     }
+    return
+  })
 
-
-    app.use('*', async (req, res) => {
-        const url = req.originalUrl
-        let template;
-        let render;
-        try {
-            if (!isProd) {
-                template = fs.readFileSync(resolve('./index.html'), 'utf-8')
-                template = await vite.transformIndexHtml(url, template)
-                render = (await vite.ssrLoadModule('./src/entry-server.js')).render
-            } else {
-                template = indexProd
-                const entryServe = await import('./dist/server/entry-server.js')
-                render = entryServe.render
-            }
-            const appHtml = await render(url)
-            // 5. 注入渲染后的应用程序 HTML 到模板中。
-            const html = template.replace(`<!--ssr-outlet-->`, appHtml)
-            // // 6. 返回渲染后的 HTML。
-            res.status(200)
-            res.setHeader('Content-Type', 'text/html')
-            res.end(html)
-        } catch (e) {
-            vite && vite.ssrFixStacktrace(e)
-            res.status(500).end(e.message)
-        }
-        return
-    })
-
-    app.listen(3099, () => {
-        console.log(3099)
-    })
+  app.listen(3099, () => {
+    console.log(3099)
+  })
 }
 
 createSeaver()
