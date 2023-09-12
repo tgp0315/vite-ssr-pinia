96fe436baf5e378acff650fd8fbeb6d805c6576a
tgp <guiguzi0315@163.com>
2023-09-12 14:30:02 +0800

    完善功能

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-</script>
-
 <template>
-  <div>
-    <router-link to="/">Home</router-link>
-    <router-link to="/about">About</router-link>
-    <router-view></router-view>
-  </div>
+    <RouterView />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style>
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,7 @@
-import { createSSRApp } from 'vue'
-import createRoute from './router'
-import createStore from './store'
-// import './style.css'
-import App from './App.vue'
+import { createSSRApp } from 'vue';
+import App from './App.vue';
 
-export const createApp =() => {
-    const app = createSSRApp(App)
-    const router = createRoute()
-    const store = createStore()
-    app.use(router)
-    app.use(store)
-    // app.mount('#app')
-    return  {
-      app,
-      router,
-      store
-    }
-}
+export const createApp = () => {
+    const app = createSSRApp(App);
+    return { app };
+};
